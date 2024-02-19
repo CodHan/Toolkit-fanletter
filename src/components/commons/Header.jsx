db7aac511e013fab1ae6fd18c1dacaf4d0778b8d
unknown <qpscm662@naver.com>
2024-02-19 17:18:36 +0900

    [style]헤더

--- src/components/commons/Header.jsx
+++ src/components/commons/Header.jsx
@@ -3,13 +3,20 @@ import * as S from '../style/commonsStyle/Header.style';
 
 function Header() {
   return (
-    <div>
+    <>
+      <S.HeaderNav>
+        <S.LinkStyle to={'/mainpage'}>HOME</S.LinkStyle>
+        <S.MYPAGE>
+          <S.LinkStyle to={'/mypage'}>MY PAGE</S.LinkStyle>
+          <S.LogOutLink to={''}>LOGOUT</S.LogOutLink>
+        </S.MYPAGE>
+      </S.HeaderNav>
       <S.HederBackGround>
         <S.HeaderTitle>
           <S.Title>대한 민국 만세 팬레터</S.Title>
         </S.HeaderTitle>
       </S.HederBackGround>
-    </div>
+    </>
   );
 }
 
--- src/components/style/commonsStyle/Header.style.jsx
+++ src/components/style/commonsStyle/Header.style.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import image from '../image/header.png';
+import { Link } from 'react-router-dom';
 export const HederBackGround = styled.header`
   background-image: url(${image});
   display: flex;
@@ -20,4 +21,29 @@ export const HeaderTitle = styled.div`
 export const Title = styled.h1`
   font-size: 30px;
   color: #cd0c22;
+`;
+
+export const HeaderNav = styled.div`
+  width: 100%;
+  height: 50px;
+  background-color: black;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 15px;
+`;
+export const LinkStyle = styled(Link)`
+  color: white;
+  font-size: 30px;
+`;
+export const LogOutLink = styled(Link)`
+  color: red;
+  font-size: 30px;
+  padding: 0 25px 0 25px;
+`;
+export const HOME = styled.div`
+  display: flex;
+`;
+export const MYPAGE = styled.div`
+  display: flex;
 `;
